ea860551b1a51b2282807d934e893d4d72b7ba62
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-03-03 19:56:04 +0000

    t Cleaning code Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -6,18 +6,16 @@
 #include <map>
 
 using namespace ApprovalTests;
-typedef double (TrigMath::*function)(double);
 
-
-TEST_CASE("Test Sin") {
+TEST_CASE("Test Sin")
+{
     TrigMath math;
-    std::map<std::string, function> functions;
+    std::map<std::string, decltype(&TrigMath::Sin)> functions;
     functions["Sin"] = &TrigMath::Sin;
     functions["Cos"] = &TrigMath::Cos;
     std::vector<double> numbers{0.5, 1.5, 2.0};
     ApprovalTests::CombinationApprovals::verifyAllCombinations(
-        [&](auto key, double d) {
-            return (math.*functions[key])(d);
-            },
-        getKeys(functions), numbers);
+        [&](auto key, double d) { return (math.*functions[key])(d); },
+        getKeys(functions),
+        numbers);
 }
